258e926cd3b2e9a82c6603029b7e8d191ff7c6a6
doha1418 <ddoohaa1234@gmail.com>
2021-03-13 19:25:39 +0000

    RstudioCommit

--- Run_Analysis.R
+++ Run_Analysis.R
@@ -1,14 +1,16 @@
 source("AnalysisScript.R")
 directories<-read.csv("../audio/Music/Directories.csv",header = TRUE,stringsAsFactors=FALSE)
 
+gradient<- c()
 
 for (dir in c(1:11)){
   print(directories[dir,"Directory"])
-  gradient<-power_spectrum_wav( file_name=directories[dir,"Directory"], start_time = 1, end_time = 7, title=directories[dir,"TrackName"])
-  print(gradient)
+  gradient<-append(gradient,power_spectrum_wav( file_name=directories[dir,"Directory"]
+                  , start_time = 1, end_time = 7, title=directories[dir,"TrackName"]))
+  
   
 }
-
+print(gradient)
 power_spectrum_wav("../audio/WhiteNoise2.wav", start_time = 1, end_time = 7, title="White Noise")
 
 power_spectrum_wav_butterLowpass("../audio/WhiteNoise2.wav", start_time = 1, end_time = 10, title="White Noise Voss and Clarke's Model")
